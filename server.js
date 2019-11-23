b36f5e7087401e83396ad2a219d0a127cb580d8e
WD$ <wyntonmdavis@gmail.com>
2019-11-23 08:52:54 -0500

    api routing

--- server.js
+++ server.js
@@ -7,3 +7,23 @@ var app = express();
 
 var databaseUrl = "scraper";
 var collections = ["scrapedData"];
+
+var db = mongojs(databaseUrl, collection);
+db.on("error", function(error) {
+    console.log("Database Error:", error)
+});
+
+app.get("/", function (req,res) {
+    res.send("Hello world");
+});
+
+app.get("/all", function (req, res) {
+    db.scrapedData.find({}, function (error, found) {
+        if(error) {
+            console.log(error);
+        }
+        else {
+            res.json(found);
+        }
+    });
+});
