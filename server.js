c9289467bd5b34e0f7ab0a3cbf7ff2d8e919495e
WD$ <wyntonmdavis@gmail.com>
2019-12-05 14:56:57 -0500

    server routing

--- server.js
+++ server.js
@@ -1,63 +1,161 @@
 var express = require("express");
-var mongojs = require("mongojs");
-var axios = require("axios");
+var method = require("method-override");
+var body = require("body-parser");
+var exphbs = require("express-handlebars");
+var mongoose = require("mongoose");
+var logger = require("morgan");
 var cheerio = require("cheerio");
+var axios = require("axios");
+
+// Mongoose
+
+var Note = require("./models/Note");
+var Article = require("./models/Article");
+var databaseUrl = 'mongodb://localhost/scrap';
+
+if (process.env.MONGODB_URI) {
+	mongoose.connect(process.env.MONGODB_URI);
+}
+else {
+	mongoose.connect(databaseUrl);
+};
+
+mongoose.Promise = Promise;
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+	console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+	console.log("Mongoose connection successful.");
+});
+
 
 var app = express();
+var port = process.env.PORT || 3000;
 
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+// app set-ups
 
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-    console.log("Database Error:", error)
+app.use(logger("dev"));
+app.use(express.static("/public"));
+app.use(body.urlencoded({extended: false}));
+app.use(method("_method"));
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.listen(port, function() {
+	console.log("Listening on port " + port);
+})
+
+// Routes
+
+app.get("/", function(req, res) {
+	Article.find({}, null, {sort: {created: -1}}, function(err, data) {
+		if(data.length === 0) {
+			res.render("placeholder", {message: "There's nothing scraped yet. Please click \"Scrape For Newest Articles\" for updated world news."});
+		}
+		else{
+			res.render("index", {articles: data});
+		}
+	});
 });
 
-app.get("/", function (req,res) {
-    res.send("Hello world");
-        axios.get("https://www.forbes.com/ai/#62ec1a757052").then(function (response) {
-
-    var $ = cheerio.load(response.data);
-
-    $(".editors-pick").each(function (i, element) {
-        var title = $(element).children("a").text();
-        var link = $(element).children("a").attr("href");
-            console.log("")
-        if (title && link) {
-            db.scrapedData.insert({
-                title:title,
-                link:link
-            },
-            function(err, inserted) {
-                if(err) {
-                    console.log(err);
-                }
-                else{
-                    console.log(inserted);
-                }
-            });
-        }
-    });
-    });
-
-    res.send("Data Scrape is Complete! Reading is Fundamental");
+app.get("/scrape", function(req, res) {
+	axios("https://www.cnn.com/world", function(error, response, html) {
+		var $ = cheerio.load(html);
+		var result = {};
+		$("h3.cd__headline").each(function(i, element) {
+			var link = $(element).find("a").attr("href");
+			var title = $(element).find("h3.cd__headline").text().trim();
+			var summary = $(element).find("span.cd__headline-text").text().trim();
+			var img = $(element).parent().parent().find("div.media").find("img").attr("src");
+			result.link = link;
+			result.title = title;
+			if (summary) {
+				result.summary = summary;
+			};
+			if (img) {
+				result.img = img;
+			}
+			else {
+				result.img = $(element).find(".wide-thumb").find("img").attr("src");
+			};
+			var entry = new Article(result);
+			Article.find({title: result.title}, function(err, data) {
+				if (data.length === 0) {
+					entry.save(function(err, data) {
+						if (err) throw err;
+					});
+				}
+			});
+		});
+		console.log("Scrape finished.");
+		res.redirect("/");
+	});
 });
 
-app.get("/all", function (req, res) {
-    db.scrapedData.find({}, function (error, found) {
-        if(error) {
-            console.log(error);
-        }
-        else {
-            res.json(found);
-        }
-    });
+app.get("/saved", function(req, res) {
+	Article.find({issaved: true}, null, {sort: {created: -1}}, function(err, data) {
+		if(data.length === 0) {
+			res.render("placeholder", {message: "You have not saved any articles yet. Click \"Save Article\" to save news of your choice!" });
+		}
+		else {
+			res.render("saved", {saved: data});
+		}
+	});
 });
 
-// app.get("/scrape", function (req, res) {
+app.get("/:id", function(req, res) {
+	Article.findById(req.params.id, function(err, data) {
+		res.json(data);
+	})
+})
 
-// });
-
-app.listen(3000, function() {
-    console.log("App running on port: 3000!");
+app.post("/search", function(req, res) {
+	console.log(req.body.search);
+	Article.find({$text: {$search: req.body.search, $caseSensitive: false}}, null, {sort: {created: -1}}, function(err, data) {
+		console.log(data);
+		if (data.length === 0) {
+			res.render("placeholder", {message: "Nothing has been found. Please try other keywords."});
+		}
+		else {
+			res.render("search", {search: data})
+		}
+	})
 });
+
+app.post("/save/:id", function(req, res) {
+	Article.findById(req.params.id, function(err, data) {
+		if (data.issaved) {
+			Article.findByIdAndUpdate(req.params.id, {$set: {issaved: false, status: "Save Article"}}, {new: true}, function(err, data) {
+				res.redirect("/");
+			});
+		}
+		else {
+			Article.findByIdAndUpdate(req.params.id, {$set: {issaved: true, status: "Saved"}}, {new: true}, function(err, data) {
+				res.redirect("/saved");
+			});
+		}
+	});
+});
+
+app.post("/note/:id", function(req, res) {
+	var note = new Note(req.body);
+	note.save(function(err, doc) {
+		if (err) throw err;
+		Article.findByIdAndUpdate(req.params.id, {$set: {"note": doc._id}}, {new: true}, function(err, newdoc) {
+			if (err) throw err;
+			else {
+				res.send(newdoc);
+			}
+		});
+	});
+});
+
+app.get("/note/:id", function(req, res) {
+	var id = req.params.id;
+	Article.findById(id).populate("note").exec(function(err, data) {
+		res.send(data.note);
+	})
+})
